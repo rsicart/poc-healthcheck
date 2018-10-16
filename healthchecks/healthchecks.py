daa41c4b9993b93afa99eccface730cca8cc1d14
Roger Sicart <roger.sicart@oxalide.com>
2018-10-16 18:37:04 +0200

    MysqlHealthcheck implementation

--- healthchecks/healthchecks.py
+++ healthchecks/healthchecks.py
@@ -1,4 +1,5 @@
 import sys
+import pymysql
 
 class Healthcheck:
 
@@ -11,14 +12,39 @@ class Healthcheck:
 
 class MysqlHealthcheck(Healthcheck):
 
-    def __init__(self, name):
+    def __init__(self, name, host='localhost', port=3306, user='root', password='', db='mysql'):
         super(Healthcheck, self).__init__()
         self.name = name
         self.type = "mysql"
+        self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+        self.db = db
 
     def run(self):
         print("Name: {} - Type: {}".format(self.name, self.type))
-        return True
+        connection = pymysql.connect(host=self.host,
+                                        port=self.port,
+                                        user=self.user,
+                                        password=self.password,
+                                        db=self.db,
+                                        charset='utf8mb4',
+                                        cursorclass=pymysql.cursors.DictCursor)
+
+        try:
+            with connection.cursor() as cursor:
+                sql = 'SELECT COUNT(1)'
+                cursor.execute(sql)
+                result = cursor.fetchone()
+
+        finally:
+            connection.close()
+
+        if result == 1:
+            return True
+
+        return False
 
 
 class RedisHealthcheck(Healthcheck):
@@ -66,7 +92,12 @@ if __name__ == '__main__':
 
     mysql_checks = settings.HEALTHCHECKS_MYSQL
     for name, check in mysql_checks.items():
-        checks.append(MysqlHealthcheck(check['name']))
+        checks.append(MysqlHealthcheck(check['name'],
+                                        host=check['host'],
+                                        port=check['port'],
+                                        user=check['username'],
+                                        password=check['password'],
+                                        db=check['db']))
 
     hc = Healthchecks(checks)
     hc.run()
